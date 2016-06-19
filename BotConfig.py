bdc0d199cd92ca36f9ffd55f1320a3bba11d090e
VSquidDevV <contact.devsquid@gmail.com>
2016-06-19 18:31:02 -0500

    Moved over to new bot framework, new storage framework included

---
+++ BotConfig.py
@@ -0,0 +1,16 @@
+#Do not change these values 
+HOST = "irc.twitch.tv"
+PORT = 6667
+WHOST = "irc.chat.twitch.tv"
+WPORT = 6667
+
+#Username of bot
+NICK = "squid_coin_bot"
+#Oauth password found here: http://www.twitchapps.com/tmi/ (connect bots account to it)
+PASS = "oauth:oju0t5n0yo3cia31ohaz8w9uqv35b2"
+#Default Channel bot will join
+CHAN = "drowsysquid75"
+#Name of currency
+CurrencyName = "SquidBux"
+#Number of decimal places to show
+CurrencyDecimals = 0
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 import socket
 import Queue
 import thread
@@ -11,14 +10,18 @@ import os
 import random
 import atexit
 from threading import Timer
-HOST = "irc.twitch.tv"
-PORT = 6667
-WHOST = "irc.chat.twitch.tv"
-WPORT = 6667
-NICK = "squid_coin_bot"
-PASS = "oauth:oju0t5n0yo3cia31ohaz8w9uqv35b2"
-CHAN = "drowsysquid75"
-currency_name = "SquidBux"
+import types
+from BotConfig import *
+class User(object):
+	def __init__(self, name):
+		self.name = name
+		self.currency = 0
+		self.time = 0
+		self.is_mod = None
+		self.is_online = True
+class Storage(object):
+	def __init__(self):
+		pass
 class Bot(object):
 	def __init__(self, nick, pasw, chan):
 		#socket for sending
@@ -35,30 +38,24 @@ class Bot(object):
 		self.chan = chan
 		self.commands = {}
 		self.chatlog = []
-		#{username: {currency:0, is_online:True or False, is_mod: True or False}} done this way so that users can add custom attributs to the user dictonary
 		self.users = {}
 		self.timers = {}
-		self.storage = {}
+		self.storage = Storage()
 		self.threads_alive = True
 		self.whisper_log = []
-	def chat(self, msg):
-		#send a chat message using the sending socket
-		self.s.send("PRIVMSG #%s : %s\r\n" %(self.chan, msg))
-	def whisper(self, target, msg):
-		self.w.send("PRIVMSG #jtv :/w %s %s\r\n" % (target, msg))
-	def connect(self, host, port):
+	def connect(self):
 		#connect the sending socket
-		self.s.connect((host, port))
+		self.s.connect((HOST, PORT))
 		self.s.send("PASS %s\r\n" %self.pasw)
 		self.s.send("NICK %s\r\n" %self.nick)
 		self.s.send("JOIN #%s\r\n" %self.chan)
 		#and the recieving socket
-		self.r.connect((host, port))
+		self.r.connect((HOST, PORT))
 		self.r.send("PASS %s\r\n" %self.pasw)
 		self.r.send("NICK %s\r\n" %self.nick)
 		self.r.send("JOIN #%s\r\n" %self.chan)
 		#and finally the whispering socket
-		self.w.connect((host, port))
+		self.w.connect((HOST, PORT))
 		self.w.send("PASS %s\r\n" %self.pasw)
 		self.w.send("NICK %s\r\n" %self.nick)
 		self.w.send("JOIN #jtv\r\n")
@@ -69,34 +66,35 @@ class Bot(object):
 		self.wr.send("NICK %s\r\n" %self.nick)
 		self.wr.send("JOIN #jtv\r\n")
 		self.wr.send("CAP REQ :twitch.tv/commands\r\n")
-		print "connected"
-	#commands must run without fail so they tried until they can be appended to the queue
-	def command(self, command, mess):
-		def command_thread(self, command, mess):
-			while self.threads_alive:
-				try:
-					self.q.put((command, mess))
-					break
-				except:
-					pass
-		thread.start_new_thread(command_thread, (self, command, mess))
+		print "Sockets connected."
+	#make the bot execute something; fails if it cannot execute it after three tries
+	def bot_command(self, command, kwargs):
+		if isinstance(kwargs, dict) and isinstance(command, types.FunctionType):
+			def command_thread(self, command, kwargs):
+				while self.threads_alive:
+					try:
+						self.q.put((command, kwargs))
+						break
+					except:
+						traceback.print_exc()
+						pass
+			thread.start_new_thread(command_thread, (self, command, kwargs))
+		else:
+			print "Command must be a function and kwargs must be a dict"
 	#start processing queued commands 
 	def start_working(self):
 		def worker(self):
 			while self.threads_alive:
 				try:
-					command, mess = self.q.get()
-					command(mess, self)
+					command, kwargs = self.q.get()
+					kwargs.update({"bot": self})
+					command(**kwargs)
 				except:
-					pass
+					traceback.print_exc()
 		thread.start_new_thread(worker, (self,))
-	#add a commmand to the list of items to be checked
-	def add_command(self, command):
-		self.commands.update({command.keyword:command.action})
-	def add_timer(self, timer, args):
-		self.timers.update({timer.name: [timer(), args]})
 	def start_recieving(self):
 		def reciever(self):
+			mess = ""
 			while self.threads_alive:
 				try:
 					mess = self.r.recv(2048)
@@ -111,10 +109,10 @@ class Bot(object):
 		thread.start_new_thread(reciever, (self,))
 	def start_recieving_whispers(self):
 		def reciever(self):
+			mess = ""
 			while self.threads_alive:
 				try:
 					mess = self.wr.recv(2048)
-					print mess
 					username = mess.split("@")[0].split("!")[1]
 					mess = mess.split("WHISPER %s :" % (self.nick))[1].split("\r\n")[0]
 					print "whisper:  "+username +" : "+ mess
@@ -124,88 +122,88 @@ class Bot(object):
 					if "PING" in mess:
 						self.s.send("PONG :tmi.twitch.tv\r\n")
 		thread.start_new_thread(reciever, (self,))
-	def start_detection(self):
-		def detection_thread(self):
-			checked = []
-			while self.threads_alive:
-				try:
-					for key in self.commands.keys():
-						#makes sure only one command per chat
-							if key in self.chatlog[0]["message"]:
-								self.command(self.commands[key], self.chatlog[0])
-								self.chatlog.remove(self.chatlog[0])
-								checked = []
-							#keeps track of all commands checked
-							if key not in self.chatlog[0]["message"]:
-								checked.append(key)
-							#if no chat commands were found in chatlog, remove it
-							if sorted(checked) == sorted(self.commands.keys()):
-								self.chatlog.remove(self.chatlog[0])
-								checked = []
-				except:
-					pass
-		thread.start_new_thread(detection_thread, (self,))
-	def start_timers(self):
-		def bot_timer(t, botself,args):
-			t.bot = botself
-			t.timer(*args)
-		for timer in self.timers:
-			thread.start_new_thread(bot_timer, (self.timers[timer][0],self,self.timers[timer][1]))
-	def load_mods(self):
+	def add_command(self, command):
+		if isinstance(command, types.ClassType):
+			try:
+				self.commands.update({command.keyword: command})
+			except:
+				print "self.keyword of class must be present when definining a command"
+		else:
+			print "Command must be a class"
+	def load_modules(self):
 		try:
 			for file in os.listdir("mods"):
-				exec(open(os.path.join("mods", file), 'r').read())
+				try:
+					exec(open(os.path.join("mods", file), 'r').read())
+				except:
+					print "Failed to exec file named %s" % (file)
 		except:
-			traceback.print_exc()
-			os.mkdir("mods")
+			try:
+				os.mkdir("mods")
+			except:
+				print "Failed to load mods. Is the folder named mods missing?"
+	def get_users(self):
+		def get_users_thread(self):
+			while self.threads_alive:
+				try:
+					users_online = json.loads(urllib2.urlopen("https://tmi.twitch.tv/group/user/%s/chatters" % CHAN).read())["chatters"]
+					for user in users_online["viewers"] + users_online["staff"] + users_online["admins"] + users_online["global_mods"]:
+						if user in self.users.keys():
+							if self.users[user].is_online:
+								pass
+							elif not self.users[user].is_online:
+								self.users[user].is_online = True
+								print "%s came online" % (user)
+							if self.users[user].is_mod:
+								self.users[user].is_mod = False
+						elif user not in self.users.keys():
+							self.users.update({user: User(user)})
+							print "New user %s" % (user)
+					for user in users_online["moderators"]:
+						if user in self.users.keys():
+							if self.users[user].is_online:
+								pass
+							elif not self.users[user].is_online:
+								self.users[user].is_online = True
+								print "%s came online" % (user)
+							if not self.users[user].is_mod:
+								self.users[user].is_mod = True
+						elif user not in self.users.keys():
+							self.users.update({user: User(user)})
+							print "New user %s" % (user)
+					for user in self.users.keys():
+						if user not in users_online["viewers"] + users_online["staff"] + users_online["admins"] + users_online["global_mods"] + users_online["moderators"]:
+							self.users[user].is_online = False
+					time.sleep(1)
+				except:
+					pass
+		thread.start_new_thread(get_users_thread, (self,))
+	def auto_save(self):
+		while self.threads_alive:
+			try:
+				pickle.dump({"storage": self.storage, "users":self.users}, open("save.p", "wb"))
+				time.sleep(15)
+			except:
+				pass
 	def load_save(self):
 		try:
-			x = pickle.load(open("save.p", 'rb'))
-			self.storage = x["storage"]
-			self.users = x["users"]
+			save = pickle.load(open("save.p", "rb"))
+			self.storage = save["storage"]
+			self.users = save["users"]
 		except:
 			pass
+	def bot_give(self, target, amount):
+		self.users[target].currency += amount
 	def start_bot(self):
 		self.load_save()
-		self.load_mods()
-		self.start_working()
+		self.load_modules()
+		self.connect()
 		self.start_recieving()
 		self.start_recieving_whispers()
-		self.start_detection()
-		self.start_timers()
-		self.chat("Squid_Coin_Bot has joined the channel! We are currently testing !bet")
-"""
-How to implement a command
-class command(object):
-	def __init__(self):
-		self.keyword = ?
-	def action(self, mess, botself):
-		pass
-"""
-"""
-How to implement a timer
-class timer(object):
-	def __init__(self):
-		self.bot = None
-	def timer(self):
-		if self.bot.threads_alive:
-			print self.bot.threads_alive
-			print "This is an example timer"
-			timer = Timer(5, self.timer)
-			thread.start_new_thread(timer.run, ())
-"""
+		self.get_users()
+		self.start_working()
+		self.auto_save()
 bot = Bot(NICK, PASS, CHAN)
-bot.connect(HOST, PORT)
 bot.start_bot()
-def this():
-	bot.threads_alive = False
-	bot.chat("Squid_Coin_Bot parting...")
-atexit.register(this)
-x = ""
-while x == "":
-	x = raw_input()
-bot.threads_alive = False
-print bot.users
-print bot.chatlog
-print bot.storage
-pickle.dump({"storage": bot.storage, "users": bot.users}, open("save.p", 'wb'))
+while True:
+	pass
